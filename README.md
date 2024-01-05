42153dcbcc2714d32ad5a7bc00b1fcd015f0f396
Odeinjul <odeinjul@gmail.com>
2024-01-05 22:11:10 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# USTC_PF_COURSES
+## Usage:
+* change ACCESS_TOKEN_HERE to your access token
+* ```python main.py```
---
+++ main.py
@@ -0,0 +1,26 @@
+import json
+import tqdm
+import requests
+
+ACCESS_TOKEN = "ACCESS_TOKEN_HERE"
+
+def get_two_grade_course(session: requests.Session):
+    path = './'
+    url = f"https://catalog.ustc.edu.cn/api/teach/lesson/list-for-teach/341?access_token={ACCESS_TOKEN}"
+    response = session.get(url)
+    data = json.loads(response.text)
+    print(data)
+
+    url = f"https://catalog.ustc.edu.cn/api/teach/lesson/infos?access_token={ACCESS_TOKEN}"
+    for course in tqdm.tqdm(data):
+        payload = f'{"{"}"codes":["{course["code"]}"],"semester":341{"}"}'
+        response = session.post(url, data=payload)
+        json_text = json.loads(response.text)
+        if (json_text[0]["grading"] == "二分制"):
+            print(json_text[0]["name"]["cn"], json_text[0]["grading"])
+            with open(path + 'two_grade_course.txt', 'a', encoding='utf-8') as fp:
+                fp.write(json_text[0]["name"]["cn"] + " " + json_text[0]["grading"] + "\n")
+
+if __name__ == '__main__':
+    session = requests.Session()
+    session = get_two_grade_course(session)

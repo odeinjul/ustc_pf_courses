3a07a9cd05a9d2d7e57c51e14d995d83abf5f732
Odeinjul <odeinjul@gmail.com>
2024-01-09 18:39:56 +0800

    upd

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import json
 import tqdm
 import requests
 
-ACCESS_TOKEN = "ACCESS_TOKEN_HERE"
+ACCESS_TOKEN = "NTkxNzU4MmYxYWRmZjU3YjkzMTZkMTVmMDQxZjgxYjZhNzI4NWRjNTcyMzNlZGQwNzRiYWZmZWU4MWRlNTk5Yw"
 SEMESTER = 341
 
 def get_two_grade_course(session: requests.Session):
@@ -10,17 +10,17 @@ def get_two_grade_course(session: requests.Session):
     url = f"https://catalog.ustc.edu.cn/api/teach/lesson/list-for-teach/{SEMESTER}?access_token={ACCESS_TOKEN}"
     response = session.get(url)
     data = json.loads(response.text)
-    print(data)
+    #print(data)
 
     url = f"https://catalog.ustc.edu.cn/api/teach/lesson/infos?access_token={ACCESS_TOKEN}"
     for course in tqdm.tqdm(data):
         payload = f'{"{"}"codes":["{course["code"]}"],"semester":{SEMESTER}{"}"}'
         response = session.post(url, data=payload)
         json_text = json.loads(response.text)
-        if (json_text[0]["grading"] == "二分制"):
-            print(json_text[0]["name"]["cn"], json_text[0]["grading"])
+        if (json_text[0]["grading"] == "二分制" or json_text[0]["examType"] == "机考"):
+            print(json_text[0]["name"]["cn"], json_text[0]["grading"], json_text[0]["examType"])
             with open(path + 'pf.txt', 'a', encoding='utf-8') as fp:
-                fp.write(json_text[0]["name"]["cn"] + " " + json_text[0]["grading"] + "\n")
+                fp.write(json_text[0]["name"]["cn"] + " " + json_text[0]["grading"] + " " + json_text[0]["examType"] + "\n")
 
 if __name__ == '__main__':
     session = requests.Session()
